f7704e4fab4314d0ca421f571d9898e0145ca873
linkinghack <linking@linkinghack.com>
2019-05-01 01:38:07 +0800

    删除微服务拆分前代码

--- code/criminalquery/src/main/java/com/linkinghack/criminalquery/exception/AddDepartmentFailedException.java
+++
@@ -1,7 +0,0 @@
-package com.linkinghack.criminalquery.exception;
-
-public class AddDepartmentFailedException extends Exception {
-    public AddDepartmentFailedException(String message) {
-        super(message);
-    }
-}
--- code/criminalquery/src/main/java/com/linkinghack/criminalquery/exception/RegisterFailedException.java
+++
@@ -1,7 +0,0 @@
-package com.linkinghack.criminalquery.exception;
-
-public class RegisterFailedException extends Exception {
-    public RegisterFailedException(String message) {
-        super(message);
-    }
-}
